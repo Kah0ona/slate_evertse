3022a09ad3dcbb10dc1719e98f4e252f2cc1236a
Lars Boesenach <lars@lokaalgevonden.nl>
2014-05-26 13:54:10 +0200

    added the app page logo in footer + added a app page template

--- dev/php/templates/footer.php
+++ dev/php/templates/footer.php
@@ -1,17 +1,38 @@
-
 	<div class="Footer">
 		<div class="u-gridContainer">
-			<?php if ( is_active_sidebar( 'footer-widgets' ) ) : ?>
-			<div class="Footer-inner u-gridRow u-gridContainer">
-				<?php dynamic_sidebar( 'footer-widgets' ); ?>
+			<div class="Footer-inner">
+				<div class="widget one">
+					<h4>Numansdorp</h4>
+					<p>Chopinstraat 1</p>
+					<p>3281 TA Numansdorp</p>
+					<p>(T) 0186-680870</p>
+					<p>Email: info@evertse.nl</p>
+				</div>
+				
+				<div class="widget two">
+					<h4>Heerjansdorp</h4>
+					<p>Kromme Nering 24</p>
+					<p>2995 BH Heerjansdam</p>
+					<p>(T) 078-6772440</p>
+					<p>Email: info@evertse.nl</p>
+				</div>
+				
+				
+				<div class="widget three">
+			      <h4>Sitemap</h4>
+			      <?php include 'includes/sitemap.php'; ?>
+			      <div class="footer-app-logo">
+					  <a href="/app-page/">
+						  <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/app-logo.png"></a>
+			      </div>
+				</div>
 			</div>
-			<?php endif; ?>
 		</div>
 	</div>
 
 	<!-- Copyright info -->
 	<div class="copyright-info">
-		<p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
+		<p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?> - Website & SEO door <a href="http://www.lokaalgevonden.nl" target="_blank">LokaalGevonden</a></p> 
 	</div>
 	
   <!-- Load jquery from google CDN if possible, with fallback to local version -->
